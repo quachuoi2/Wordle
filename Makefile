ab3e50d7699f68cb647768c784903401a468472a
qnguyen <qnguyen@student.42.fr>
2022-02-24 08:27:16 +0200

    combined check green silver yellow into one function, took care of unfreed memory

--- Makefile
+++ Makefile
@@ -6,14 +6,14 @@
 #    By: qnguyen <qnguyen@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/02/23 08:33:35 by qnguyen           #+#    #+#              #
-#    Updated: 2022/02/23 23:25:26 by qnguyen          ###   ########.fr        #
+#    Updated: 2022/02/24 08:26:08 by qnguyen          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 NAME = wordle
 SRC = get_next_line.c main.c utilities.c check_functions.c
 
-all: #libft.a
+all: libft.a
 	@gcc -g $(SRC) -Llibft/ -lft -Ilibft/ #-o wordle
 
 libft.a:
--- check_functions.c
+++ check_functions.c
@@ -6,51 +6,13 @@
 /*   By: qnguyen <qnguyen@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/23 23:24:29 by qnguyen           #+#    #+#             */
-/*   Updated: 2022/02/24 07:37:30 by qnguyen          ###   ########.fr       */
+/*   Updated: 2022/02/24 08:18:43 by qnguyen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "wordle.h"
 
-int		check_green(char *possible_word, char *input_word, char *color)
-{
-	int	i;
-
-	i = 0;
-	while (possible_word[i])
-	{
-		if (color[i] == 'g')
-			if ((possible_word[i] != input_word[i]))
-				return (0);
-		i++;
-	}
-	return (1);
-}
-
-int		check_silver(char *possible_word, char *input_word, char *color)
-{
-	int	i;
-	int	i2;
-
-	i = 0;
-	while (input_word[i])
-	{
-		i2 = 0;
-		if (color[i] == 's')
-		{
-			while (possible_word[i2])
-			{
-				if (possible_word[i2] == input_word[i] && color[i2] != 'g')
-					return (0);
-				i2++;
-			}
-		}
-		i++;
-	}
-	return (1);
-}
-
-int		check_yellow(char *possible_word, char *input_word, char *color)
+int		check_gsy(char *possible_word, char *input_word, char *color)
 {
 	int	i;
 	int	i2;
@@ -59,18 +21,23 @@ int		check_yellow(char *possible_word, char *input_word, char *color)
 	i = 0;
 	while (input_word[i])
 	{
+		if (color[i] == 'g')
+			if ((possible_word[i] != input_word[i]))
+				return (0);
 		i2 = 0;
-		if (color[i] == 'y')
+		present = 0;
+		while (possible_word[i2])
 		{
-			while (possible_word[i2])
-			{
+			if (color[i] == 's')
+				if (possible_word[i2] == input_word[i] && color[i2] != 'g')
+					return (0);
+			if (color[i] == 'y')
 				if (possible_word[i2] == input_word[i] && i != i2)
 					present = 1;
-				i2++;
-			}
-			if (present == 0)
-				return (0);
+			i2++;
 		}
+		if (present == 0 && color[i] == 'y')
+			return (0);
 		i++;
 	}
 	return (1);
@@ -85,9 +52,7 @@ void	check_word(char (*w_list)[TOTAL_WORDS + 1][6], char (*after_list)[TOTAL_WOR
 	i2 = 0;
 	while ((*w_list)[i][0] != '\0')
 	{
-		if (check_green((*w_list)[i], word, color)
-			&& check_silver((*w_list)[i], word, color)
-			&& check_yellow((*w_list)[i], word, color))
+		if (check_gsy((*w_list)[i], word, color))
 			ft_strcpy((*after_list)[i2++], (*w_list)[i]);
 		i++;
 	}
